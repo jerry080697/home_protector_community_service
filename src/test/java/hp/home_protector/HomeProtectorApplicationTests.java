96d8765658e0542500e990a74f31e59be643bdb9
seoshinehyo <springssh0504@naver.com>
2025-03-20 15:22:15 +0900

    ✨  feature: first commit

---
+++ src/test/java/hp/home_protector/HomeProtectorApplicationTests.java
@@ -0,0 +1,13 @@
+package hp.home_protector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeProtectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
